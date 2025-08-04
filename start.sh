b076363441be439d7cc3eba59a8ae458a730757e
老王 <sun@f4i.cn>
2025-08-04 18:27:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ else
     exit 1
 fi
 
-BASE_URL="https://github.com/eooce/webssh/releases/download/latest"
+BASE_URL="https://github.com/eooce/webssh/releases/latest/download"
 FILE_URL="${BASE_URL}/webssh_linux_${ARCH}"
 
 # 如果文件不存在，则下载
